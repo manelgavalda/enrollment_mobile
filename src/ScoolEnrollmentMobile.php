1aff9024f22cfa4c691bfbca8d549eba07ad7eef
manel <manelgavalda@iesebre.com>
2016-12-01 21:17:15 +0100

    Added enrollment table

---
+++ src/ScoolEnrollmentMobile.php
@@ -0,0 +1,20 @@
+<?php
+namespace Scool\EnrollmentMobile;
+
+class ScoolEnrollmentMobile
+{
+//    public static function factories()
+//    {
+//        return [
+//            SCOOL_ENROLLMENT__MOBILE_PATH . '/database/factories/ClassroomFactory.php' =>
+//                database_path('/factories/ClassroomFactory.php'),
+//        ];
+//    }
+//    public static function configs()
+//    {
+//        return [
+//            SCOOL_CURRICULUM_PATH . '/config/curriculum.php' =>
+//                config_path('curriculum.php'),
+//        ];
+//    }
+}
