e2da69939a10ad526dac6a780cb5b2bc4889d205
manel <manelgavalda@iesebre.com>
2016-12-13 17:07:57 +0100

    route import

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,10 +1,14 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
 Route::group([
     'middleware' => 'web'], function() {
     Route::group(['middleware' => 'auth'], function () {
         Route::resource('enrollments', 'EnrollmentsController');
     });
 });
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'api',
