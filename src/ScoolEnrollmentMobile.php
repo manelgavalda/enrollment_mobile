1a3add4b0d82530263ccbcbacb81f67c2bcfe18c
manel <manelgavalda@iesebre.com>
2016-12-13 16:15:38 +0100

    Added route file

--- src/ScoolEnrollmentMobile.php
+++ src/ScoolEnrollmentMobile.php
@@ -1,20 +1,45 @@
 <?php
 namespace Scool\EnrollmentMobile;
 
+/**
+ * Class ScoolEnrollmentMobile
+ * @package Scool\EnrollmentMobile
+ */
 class ScoolEnrollmentMobile
 {
-    //    public static function factories()
-//    {
-//        return [
-//            SCOOL_ENROLLMENT__MOBILE_PATH . '/database/factories/ClassroomFactory.php' =>
-//                database_path('/factories/ClassroomFactory.php'),
-//        ];
-//    }
-//    public static function configs()
-//    {
-//        return [
-//            SCOOL_CURRICULUM_PATH . '/config/curriculum.php' =>
-//                config_path('curriculum.php'),
-//        ];
-//    }
+    /**
+     * @return array
+     */
+    public static function factories()
+    {
+        return [
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/EnrollmentMobileFactory.php' =>
+                database_path('/factories/EnrollmentMobileFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/CycleFactory.php' =>
+//                database_path('/factories/CycleFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/DepartmentFactory.php' =>
+//                database_path('/factories/DepartmentFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/LawFactory.php' =>
+//                database_path('/factories/LawFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/ModuleFactory.php' =>
+//                database_path('/factories/ModuleFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SpecialityFactory.php' =>
+//                database_path('/factories/SpecialityFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/StudyFactory.php' =>
+//                database_path('/factories/StudyFactory.php'),
+//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SubmoduleFactory.php' =>
+//                database_path('/factories/SubmoduleFactory.php'),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public static function configs()
+    {
+        return [
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/config/enrollment_mobile.php' =>
+                config_path('enrollment_mobile.php'),
+        ];
+    }
 }
