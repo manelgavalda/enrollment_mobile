1867816dcda4766d128b1df25639374c337e4eff
manel <manelgavalda@iesebre.com>
2016-12-15 20:20:35 +0100

    Nameable

--- src/Models/Enrollment.php
+++ src/Models/Enrollment.php
@@ -2,6 +2,7 @@
 
 namespace Scool\EnrollmentMobile\Models;
 
+use Acacha\Names\Traits\Nameable;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
@@ -12,6 +13,8 @@ use Prettus\Repository\Traits\TransformableTrait;
  */
 class Enrollment extends Model implements Transformable
 {
-    use TransformableTrait;
+    use TransformableTrait, Nameable;
     //
+
+    protected $fillable['name'];
 }
