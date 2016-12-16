abc55c85c28cb877d30a04df055fbab02d6db89d
manelgavalda <manelgavalda@iesebre.com>
2016-12-16 23:49:37 +0100

    return factories

--- src/ScoolEnrollmentMobile.php
+++ src/ScoolEnrollmentMobile.php
@@ -16,6 +16,22 @@ class ScoolEnrollmentMobile
         return [
             SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/EnrollmentFactory.php' =>
                 database_path('/factories/EnrollmentFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/ClassroomFactory.php' =>
+                database_path('/factories/ClassroomFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/CycleFactory.php' =>
+                database_path('/factories/CycleFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/DepartmentFactory.php' =>
+                database_path('/factories/DepartmentFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/LawFactory.php' =>
+                database_path('/factories/LawFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/ModuleFactory.php' =>
+                database_path('/factories/ModuleFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SpecialityFactory.php' =>
+                database_path('/factories/SpecialityFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/StudyFactory.php' =>
+                database_path('/factories/StudyFactory.php'),
+            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SubmoduleFactory.php' =>
+                database_path('/factories/SubmoduleFactory.php'),
         ];
     }
 
