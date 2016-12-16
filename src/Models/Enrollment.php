a1df79b9121320502cf57582fb8650c9f89723c9
manelgavalda <manelgavalda@iesebre.com>
2016-12-16 23:53:34 +0100

    Added Stateful Trait

--- src/Models/Enrollment.php
+++ src/Models/Enrollment.php
@@ -2,16 +2,21 @@
 
 namespace Scool\EnrollmentMobile\Models;
 
+use Acacha\Names\Traits\Nameable;
+use Acacha\Stateful\Traits\StatefulTrait;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
 /**
  * Class Enrollment
- * @package Scool\EnrollmentMobile\Models
+ * @package App\Entities
  */
 class Enrollment extends Model implements Transformable
 {
-    use TransformableTrait;
-    //
+    //afegim model stateful per als models que volem qeu tinguin estat, com en el name per als noms. Afegir columna state ala migracio de l'objecte(i ens dira com esta l'objecte de la taula(open,closed,etc), es configurable. state amb nullabel si pot ser que no es guarde
+    use TransformableTrait,Nameable,StatefulTrait;
+
+    protected $fillable = ['name'];
+    //TODO: mirar estats (enrollment). Implementar i definir estats(exemple porta(esborrany,valida,feta).
 }
