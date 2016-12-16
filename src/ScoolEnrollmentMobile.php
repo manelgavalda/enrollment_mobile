f8d0aa4b7b8d6b9620c4cc90ecac43026ba1b2fa
manelgavalda <manelgavalda@iesebre.com>
2016-12-16 22:14:20 +0100

    ScoolEnrollmentMobile refractor

--- src/ScoolEnrollmentMobile.php
+++ src/ScoolEnrollmentMobile.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Scool\EnrollmentMobile;
 
 /**
@@ -15,20 +16,6 @@ class ScoolEnrollmentMobile
         return [
             SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/EnrollmentFactory.php' =>
                 database_path('/factories/EnrollmentFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/CycleFactory.php' =>
-//                database_path('/factories/CycleFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/DepartmentFactory.php' =>
-//                database_path('/factories/DepartmentFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/LawFactory.php' =>
-//                database_path('/factories/LawFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/ModuleFactory.php' =>
-//                database_path('/factories/ModuleFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SpecialityFactory.php' =>
-//                database_path('/factories/SpecialityFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/StudyFactory.php' =>
-//                database_path('/factories/StudyFactory.php'),
-//            SCOOL_ENROLLMENT_MOBILE_PATH . '/database/factories/SubmoduleFactory.php' =>
-//                database_path('/factories/SubmoduleFactory.php'),
         ];
     }
 
