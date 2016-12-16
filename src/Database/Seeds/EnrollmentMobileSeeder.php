87a2ecf236fe244316784d069c9f58c95603f4ea
manelgavalda <manelgavalda@iesebre.com>
2016-12-16 23:47:46 +0100

    Seeder

--- src/Database/Seeds/EnrollmentMobileSeeder.php
+++ src/Database/Seeds/EnrollmentMobileSeeder.php
@@ -2,7 +2,14 @@
 
 namespace Scool\EnrollmentMobile\Database\Seeds;
 
+
 use Illuminate\Database\Seeder;
+use Scool\EnrollmentMobile\Models\Classroom;
+use Scool\EnrollmentMobile\Models\Speciality;
+use Scool\EnrollmentMobile\Models\Submodule;
+use Scool\EnrollmentMobile\Models\Module;
+use Scool\EnrollmentMobile\Models\Course;
+//use Acacha\Periods\Period;
 
 /**
  * Class EnrollmentSeeder
