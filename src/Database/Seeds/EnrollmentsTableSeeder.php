2c3115e58a2e134fe1af222e2e6569ca128e189a
manelgavalda <manelgavalda@iesebre.com>
2016-12-17 20:05:23 +0100

    Seeder to factory with faker

--- src/Database/Seeds/EnrollmentsTableSeeder.php
+++ src/Database/Seeds/EnrollmentsTableSeeder.php
@@ -14,25 +14,6 @@ class EnrollmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //$this->createEnrollment("Enrollment1");
         factory(Enrollment::class, 20)->create();
     }
-
-
-    private function createEnrollment($name)
-    {
-        $enrollment = [
-            'name' => $name,
-            'validated' => true, //només les vàlides són actives.
-            'finished' => false, //indica si la matricula està finalitzada.
-            //$table->integer('period_id'); //De moment descartat //obligatori
-            'study_id' => 1,
-            'course_id' => 1,
-            'classroom_id' => 1
-            //timestamps
-        ];
-
-        Enrollment::create($enrollment);
-    }
-
 }
