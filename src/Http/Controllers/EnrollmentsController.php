27b15024f7b634d7d3354b4813616fe3436bf1bb
manelgavalda <manelgavalda@iesebre.com>
2017-01-21 23:12:49 +0100

    EnrollmentsController refractor

--- src/Http/Controllers/EnrollmentsController.php
+++ src/Http/Controllers/EnrollmentsController.php
@@ -49,7 +49,6 @@ class EnrollmentsController extends Controller
     {
         $this->repository->pushCriteria(app('Prettus\Repository\Criteria\RequestCriteria'));
         $enrollments = $this->repository->all();
-        dd(response()->json($enrollments));
         if (request()->wantsJson()) {
             return response()->json([
                 'data' => $enrollments,
