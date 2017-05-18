4e34cd46b9fbefe7e07beef9004b841ff8fe0fd6
manelgavalda <manelgavalda@iesebre.com>
2017-05-18 23:51:34 +0200

    Factory seeding relationships

--- database/factories/ClassroomFactory.php
+++ database/factories/ClassroomFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Scool\EnrollmentMobile\Models\Classroom::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->word
+        'name' => $faker->word,
+        'enrollment_id' => $faker->unique()->numberBetween($min = 1, $max = 20)
     ];
 });
--- database/factories/EnrollmentFactory.php
+++ database/factories/EnrollmentFactory.php
@@ -10,6 +10,6 @@ $factory->define(Scool\EnrollmentMobile\Models\Enrollment::class, function (Fake
         'finished' => $faker->boolean, //indica si la matricula està finalitzada.
         'study_id' => $faker->randomDigit,
         'course_id' => $faker->randomDigit,
-        'classroom_id' => $faker->randomDigit
     ];
 });
+
