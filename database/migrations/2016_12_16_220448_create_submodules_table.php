5322302592954992169bd01f8c4fb4a5732d4b29
manelgavalda <manelgavalda@iesebre.com>
2017-06-05 20:24:29 +0200

    submodule total hours date to integer

--- database/migrations/2016_12_16_220448_create_submodules_table.php
+++ database/migrations/2016_12_16_220448_create_submodules_table.php
@@ -22,7 +22,7 @@ class CreateSubmodulesTable extends Migration
             $table->boolean('selected')->nullable();
             $table->unsignedTinyInteger('order')->nullable();
             $table->integer('type')->unsigned()->default(1);
-            $table->date('total_hours')->nullable();
+            $table->integer('total_hours')->nullable();
             $table->date('week_hours')->nullable();
             $table->integer('module_id')->unsigned();
             $table->date('start_date')->nullable();
